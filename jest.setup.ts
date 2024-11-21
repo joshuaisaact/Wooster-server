dae76dd207278f93ff0234571445037db5c828a9
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-11-21 06:32:55 +0000

    test: migrating to sqlite in-mem testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,9 +1,20 @@
-import cleanupTestData from './src/tests/utils/test-cleanup';
+import {
+  activities,
+  db,
+  destinations,
+  itineraryDays,
+  savedDestinations,
+  trips,
+} from './src/db';
 
 beforeEach(async () => {
   jest.clearAllMocks();
 });
 
 afterEach(async () => {
-  await cleanupTestData();
+  await db.delete(trips);
+  await db.delete(itineraryDays);
+  await db.delete(activities);
+  await db.delete(destinations);
+  await db.delete(savedDestinations);
 });
