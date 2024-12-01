235256ee1fb1525db123a3637efc0d56f0533b5a
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-12-01 07:12:54 +0000

    chore: removed unused test db schema

--- src/db/test/index.ts
+++
@@ -1,5 +0,0 @@
-export { activities } from './activities';
-export { destinations } from './destinations';
-export { itineraryDays } from './itinerary_days';
-export { savedDestinations } from './saved_destinations';
-export { trips } from './trips';
